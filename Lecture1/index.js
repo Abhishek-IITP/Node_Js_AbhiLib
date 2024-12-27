d368adcbb792c33bd182407b0b070116a8158faa
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-12-27 22:11:20 +0530

    new commit

---
+++ Lecture1/index.js
@@ -0,0 +1,22 @@
+// console.log(process.env)
+// console.log(process.argv)
+
+
+// let num1=process.argv[2];
+// let num2=process.argv[3];
+// console.log(Number(num1)+Number(num2));
+
+
+// console.log('first')
+// process.stdout.write("Hello World\n");
+// process.stdout.write("Hello jsr\n");
+
+// console.log('first')
+
+// process.exit()
+
+// console.log('second')
+
+
+console.log(__dirname)
+console.log(__filename)
